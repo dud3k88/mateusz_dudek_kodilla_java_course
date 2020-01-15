00dbfd5ec5b2ab56e4e9591264da3fd6608151c4
Mateusz Dudek <mateuszdudek@MacBook-Air-Mateusz.local>
2020-01-15 21:19:08 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6 . Wprowadzenie to testowania oprogramowania");
-    }
+
+        Calculator calculator = new Calculator(30,40);
+
+        int addResult = calculator.add();
+        int substractResult = calculator.substract();
+
+
+        if (addResult == calculator.getA()+calculator.getB()){
+            System.out.println("works correctly");
+        }else
+            System.out.println("works incorrectly");
+
+        if (substractResult == calculator.getA()-calculator.getB()){
+            System.out.println("works correctly");
+        }else
+            System.out.println("works incorrectly");
+        }
+
+
 }
