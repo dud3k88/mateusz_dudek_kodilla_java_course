7fabfe141c74b6476500e8b19370ab5528cbdf2c
Mateusz Dudek <mateuszdudek@MacBook-Air-Mateusz.local>
2020-01-22 22:39:42 +0100

    zadanie 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,16 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
 public class StreamMain {
         public static void main(String[] args) {
             System.out.println("Welcome to module 7 - Stream");
+
+            PoemBeautifier poemBeautifier = new PoemBeautifier();
+            poemBeautifier.beautify("Sample Text", String::toUpperCase);
+            poemBeautifier.beautify("Sample Text", txt -> "ABC " + txt + " ABC");
+            poemBeautifier.beautify("Sample Text", String::toLowerCase);
+            poemBeautifier.beautify("Sample Text", txt -> txt + "!!!!!!!!!!!!!!");
+
         }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+
+
+    public void beautify(String txt, PoemDecorator poemDecorator) {
+        String result = poemDecorator.decorate(txt);
+        System.out.println(result);
+    }
+
+}
